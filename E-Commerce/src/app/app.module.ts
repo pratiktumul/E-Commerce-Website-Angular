34e4d4661963b9baecb68d67ad1ccd7f481caa57
Maitri Mishra <maitri29mishra@gmail.com>
2021-06-21 10:47:00 +0530

    Admin Component

--- E-Commerce/src/app/app.module.ts
+++ E-Commerce/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Shared/navbar/navbar.component';
+import { AdminComponent } from './admin/admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
