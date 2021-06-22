d616123407e63e6ff71a0331f7b79a0f66f73ed9
Maitri Mishra <maitri29mishra@gmail.com>
2021-06-22 12:38:27 +0530

    Added Login Component in navbar folder

--- E-Commerce/src/app/app-routing.module.ts
+++ E-Commerce/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ForgotPasswordComponent } from './Shared/forgot-password/forgot-password.component';
-import { LoginComponent } from './Shared/login/login.component';
+import { ForgotPasswordComponent } from './Shared/navbar/forgot-password/forgot-password.component';
+import { LoginComponent } from './Shared/navbar/login/login.component';
 
 const routes: Routes = [
   { path: 'login' , component:LoginComponent},
